b3ef68e842ac5c557f94c345116bc30c5da3033d
Pedro Luiz <pedroluiz0013@gmail.com>
2020-07-29 19:19:48 -0300

    adicionando exercicios

---
+++ Estrutura Sequencial/ex03.py
@@ -0,0 +1,6 @@
+# ex03     Faça um Programa que peça dois números e imprima a soma.
+
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+print(f'A soma dos dois números resulta em: {n1+n2}')
