111bbe41bc8c86e1825c2f0690d8ddc9413b68ac
Pedro Luiz <pedroluiz0013@gmail.com>
2020-07-29 19:19:30 -0300

    melhorando comentários

--- Estrutura Sequencial/ex02.py
+++ Estrutura Sequencial/ex02.py
@@ -1,4 +1,4 @@
-''' Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]. '''
+# ex02   Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
 
 numero = int(input('Digite um número: '))
 print(f'O numero informado foi {numero}')
