03e0e3bd0ead8c2b7c8bba617f8fc90af595221d
Pedro Luiz <pedroluiz0013@gmail.com>
2020-07-29 21:32:26 -0300

    commit

---
+++ Estrutura Sequencial/ex05.py
@@ -0,0 +1,4 @@
+# ex05 Faça um Programa que converta metros para centímetros.
+
+metros = int(input('Comprimento em metros: '))
+print(f'{metros} metros são {metros * 100}')
---
+++ Estrutura Sequencial/ex06.py
@@ -0,0 +1,3 @@
+# ex06 Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+raio = int(input('Digite o raio de um círculo: '))
